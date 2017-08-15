9e10889997463bc63daae92962035f5a8535475e
TSpruill <tivona.spruill@yahoo.com>
2017-08-14 21:16:42 -0400

    edit with chalk

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ moment().format();
 const chalk = require('chalk');
 
 // console.log(chalk.blue('Hello world!'));
+let today = moment().dayOfYear(Number);
+console.log(`It is ${chalk.blue(today)}.`);
 
-var now = moment();
-console.log(now);
+let howLong = moment().dayOfYear(DDD);
+console.log(`${howLong}`)
+
+let seconds = moment().milliseconds();
+console.log(`It is ${chalk.blue(seconds * 1000)} seconds into the day.`);
+
+
+let dayLight = moment().isDST();
+  if (dayLight == "true"){
+console.log(`It ${chalk.green("is")} during Daylight Savings Time. `);
+  } else{
+  console.log(`It ${chalk.red("is not")} during Daylight Savings Time.`);
+    }
+
+
+let year = moment([2017]).isLeapYear();
+  if (year == "true"){
+console.log(`It ${chalk.green ("is")} a leap year. `);
+  } else{
+  console.log(`It ${chalk.red("is not")} a leap year.`)
+    }
